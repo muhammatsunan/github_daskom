d4d37b876dcede1a6e0a0a2b544f8a93f1fe690f
sunan <muhammatsunan@gmail.com>
2021-05-22 09:34:33 +0700

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ class HomeController extends Controller
 {
     public function index(){
         return view("home0159");
+        return view("home");
     }
     public function page2(){
         return view("page2");
